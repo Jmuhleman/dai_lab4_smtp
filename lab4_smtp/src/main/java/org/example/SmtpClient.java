f7fb9c53020cc979ced0ee210abc8033681b3c3a
Jmuhleman <>
2023-11-23 11:18:47 +0100

    add smtp

--- lab4_smtp/src/main/java/org/example/SmtpClient.java
+++ lab4_smtp/src/main/java/org/example/SmtpClient.java
@@ -12,20 +12,44 @@ public class SmtpClient {
              var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
              var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))){
 
-            out.write("EHLO localhost\n");
-            out.flush();
+                System.out.println(in.readLine());
+                out.write("EHLO localhost\n");
+                out.flush();
 
-            StringBuilder textResponse = new StringBuilder();
-            String response = "";
-            int index = -1;
+                String[] msgToBeSent = new String[4];
 
-            while (index == -1) {
-                index = response.indexOf(" ");
+                msgToBeSent[0] = "MAIL FROM: <shakira@music.com>";
+                msgToBeSent[1] = "RCPT TO: <shakira@music.com>";
+                msgToBeSent[2] = "DATA Bonjour, " +
+                        "adsad" +
+                        "da" +
+                        "a\r\n.\r\n";
+                msgToBeSent[3] = "QUIT";
+
+
+                StringBuilder textResponse = new StringBuilder();
+                String response;
+
+                int idx=0;
                 response = in.readLine();
-                textResponse.append(response).append("\n");
-            }
 
-            System.out.println(textResponse);
+                System.out.println(response);
+
+                while (idx != -1){
+
+                    response = in.readLine();
+                    idx = response.indexOf('-');
+                    textResponse.append(response).append("\n");
+                }
+                System.out.println(textResponse);
+
+                for (int k = 0 ; k < 4 ; ++k) {
+                    out.write(msgToBeSent[k] + "\n");
+                    out.flush();
+                    response = in.readLine();
+                    System.out.println(response);
+
+                }
             }
             catch (IOException e){
 
