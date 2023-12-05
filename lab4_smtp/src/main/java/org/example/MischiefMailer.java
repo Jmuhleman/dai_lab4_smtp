4c0c7ad4754407c8a1a4ab8f1a9a1bb0e2bd85ca
CristhianRo <114008453+CristhianRo@users.noreply.github.com>
2023-12-05 22:26:40 +0100

    modif paramètres

--- lab4_smtp/src/main/java/org/example/MischiefMailer.java
+++ lab4_smtp/src/main/java/org/example/MischiefMailer.java
@@ -5,21 +5,28 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MischiefMailer {
+    enum Parameters {NB_GROUPS, MAILS_FILE, MESSAGES_FILE, IP_ADDRESS, PORT}
     public static void main(String[] args) throws IOException {
 
-        if (args.length < 3){
-            System.out.println("Vous devez donner un nombre de groupe !\n");
-            System.out.println("Le programme va se terminer\n");
+        if (args.length < 5){
+            System.out.println("You have to give 5 arguments!");
+            System.out.println("1. You must give a group number!");
+            System.out.println("2. You must give a mail file!");
+            System.out.println("3. You must give a message file!");
+            System.out.println("4. You must give an IP address!");
+            System.out.println("5. You must give a port!");
+            System.out.println("The program will finish now.");
             return;
         }
 
-        FileHandler fh = new FileHandler(args[1], args[2], StandardCharsets.UTF_8);
+        FileHandler fh = new FileHandler(args[Parameters.MAILS_FILE.ordinal()], args[Parameters.MESSAGES_FILE.ordinal()], StandardCharsets.UTF_8);
 
         Random ran = new Random();
-        SMTPManager SMTPm = new SMTPManager("localhost", 1025);
+        SMTPManager SMTPm = new SMTPManager(args[Parameters.IP_ADDRESS.ordinal()], Integer.parseInt(args[Parameters.PORT.ordinal()]));
+
 
         // pour chaque groupe qui va être formé
-        for (int k = 0; k < Integer.parseInt(args[0]); ++k) {
+        for (int k = 0; k < Integer.parseInt(args[Parameters.NB_GROUPS.ordinal()]); ++k) {
             //tirer un nombre aléatoire [2-5] ->x
             int nVictims = ran.nextInt(2, 6);
 
